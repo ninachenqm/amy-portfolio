786a4584e955d43459f9f7e19fa32a803de68e2d
Nina <ninachenqm@gmail.com>
2025-05-16 21:23:59 -0500

    App.jsx: 移除了 container 和 padding，让页面组件自己控制

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
+// src/App.jsx
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
+// import AboutPage from './pages/AboutPage'; // 不再需要导入
 import DanceGalleryPage from './pages/DanceGalleryPage';
 import ArtGalleryPage from './pages/ArtGalleryPage';
-import SpeechesPage from './pages/SpeechesPage'; 
+import SpeechesPage from './pages/SpeechesPage';
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
       <Navbar />
-      <main className="flex-grow container mx-auto px-4 py-8"> 
+      <main className="flex-grow"> {/* 移除了 container 和 padding，让页面组件自己控制 */}
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
           <Route path="/dance" element={<DanceGalleryPage />} />
           <Route path="/art" element={<ArtGalleryPage />} />
           <Route path="/speeches" element={<SpeechesPage />} />
-          {/* later add a 404 Page */}
-          {/* <Route path="*" element={<NotFoundPage />} /> */}
         </Routes>
       </main>
       <Footer />
